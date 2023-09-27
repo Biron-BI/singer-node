a6e93cb098785aeefd55e5ef977b400fd772d483
Antoine <antoine@biron-analytics.com>
2023-09-27 11:39:41 +0200

    fix utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,7 @@
-import {List, Record} from "immutable"
 import {Command, Option} from "commander"
 import * as fs from "fs"
 import {Catalog} from "./Catalog"
-import {StateProps} from "./bookmarks"
 
-export const StateFactory = Record<StateProps>({
-  bookmarks: {},
-})
 export function load_json(filePath: string) {
   return JSON.parse(fs.readFileSync(filePath).toString())
 }
@@ -16,7 +11,7 @@ interface CustomOption {
   description: string
 }
 
-export function parse_args(required_keys: List<string>, additionalOptions: List<CustomOption> = List()) {
+export function parse_args(required_keys: string[], additionalOptions: CustomOption[] = []) {
   const program = new Command()
 
   program
@@ -26,7 +21,7 @@ export function parse_args(required_keys: List<string>, additionalOptions: List<
     .option("-d | --discover", "Do schema discovery")
 
   additionalOptions.forEach((additionalOption) =>
-    program.addOption(new Option(additionalOption.flags, additionalOption.description))
+    program.addOption(new Option(additionalOption.flags, additionalOption.description)),
   )
 
   program.parse(process.argv)
@@ -44,13 +39,16 @@ export function parse_args(required_keys: List<string>, additionalOptions: List<
     state_path: opts.state,
     catalog_path: opts.catalog,
     config: load_json(opts.config),
-    state: opts.state ? StateFactory(load_json(opts.state)) : StateFactory(),
+    state: {
+      bookmarks: {},
+      ...(opts.state ? load_json(opts.state) : {})
+    },
     catalog: opts.catalog ? Catalog.fromJSON(load_json(opts.catalog)) : undefined,
-    opts
+    opts,
   }
 
   const missing_keys = required_keys.filter((key) => !Object.keys(ret.config).includes(key));
-  if (!missing_keys.isEmpty()) {
+  if (missing_keys.length > 0) {
     throw new Error(`Config is missing required keys: ${missing_keys}`)
   }
 
